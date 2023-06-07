32b60d3fee8e9a0f9070547275a133e074bb27a5
Daffa Randika <daffa.randika.3@gmail.com>
2023-06-07 16:35:43 +0700

    Environment setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { env } from 'src/environment/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AstronomyPictureOfTheDay';
+  key = env.key
 }
